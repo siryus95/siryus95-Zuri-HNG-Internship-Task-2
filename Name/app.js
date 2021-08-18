4c2d7f632f6d4f9235c7645b1c9c77cfefd22e8f
siryus95 <salaudeenyusuf95@gmail.com>
2021-08-18 22:22:35 +0100

    Resume Page

---
+++ Name/app.js
@@ -0,0 +1,9 @@
+// Task 2 No. 2 code 
+// the name display on index.html page
+const firstName = " Yusuf" ;
+const lastName = " Salaudeen";
+
+const fullName = firstName + lastName;
+
+var myName=document.getElementById("name");
+myName.innerHTML= fullName;
